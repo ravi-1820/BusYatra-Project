a0a6a670f8bc28d192c27476764de6c081c4dd3f
Suthar Ravikumar k <ravisuthar1820@gmail.com>
2026-08-12 10:32:23 +0530

    Bus-Yatra

--- Bus/urls.py
+++ Bus/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     #Customer URL patterns
+    path('', views.index, name='home'),
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
